76e900499817fe3442ee707abce74cc5900a3c87
Brennan Davis <brennantd@gmail.com>
2022-09-26 13:10:19 -0600

    more styling on unenroll button on myprograms page:

--- client/src/components/MyPrograms.js
+++ client/src/components/MyPrograms.js
@@ -6,7 +6,10 @@ function MyPrograms({ currentUser, myPrograms }) {
     const programCards = myPrograms.map((program) => {
 
         return <div class='flex flex-col items-center'>
-            <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Unenroll</button>
+            <div class='w-3/4 pt-2 flex justify-end'>
+                <button type="button" class="focus:outline-none text-white bg-red-700 
+            hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Unenroll</button>
+            </div>
             <ProgramCard key={program.id} program={program} />
         </div>
     })
