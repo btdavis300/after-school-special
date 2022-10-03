9fc5891b7d24bcbfb3ce6d202724421ad577ec93
Brennan Davis <brennantd@gmail.com>
2022-10-03 06:26:08 -0600

    alert messages at fixed position

--- client/src/components/ASFooter.js
+++ client/src/components/ASFooter.js
@@ -27,7 +27,7 @@ function ASFooter() {
                 </ul>
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><a href="https://github.com/btdavis300/after-school-special" target="_blank" class="hover:underline">Github Repo Here</a>.
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><a href="https://github.com/btdavis300/after-school-special" target="_blank" class="hover:underline">Github Repo.</a>
             </span>
         </footer>
 
